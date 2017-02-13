ef3fd875e503f0511ac60c91789fbb0af760b19e
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2017-02-13 16:19:31 -0600

    Moved commands into classes

--- classes/commands/StartCommand.php
+++ classes/commands/StartCommand.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Commands;
+namespace App\commands;
 
 use Telegram\Bot\Actions;
 use Telegram\Bot\Commands\Command;
--- index.php
+++ index.php
@@ -7,15 +7,18 @@ require_once './vendor/autoload.php';
 
 use Telegram\Bot\Api;
 use App\BotAuth;
+use App\commands\StartCommand;
 
 
 $auth = new BotAuth();
 $telegram = new Api($auth->getAuthToken());
 
 
-$telegram->addCommand(Commands\StartCommand::class);
+$telegram->addCommand(StartCommand::class);
 
 $updates = $telegram->getWebHookUpdates();
+
+echo "test";
 
 // $response = $telegram->getMe();
 
