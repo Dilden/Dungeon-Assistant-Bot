bd4b0c84b4f328376ae9bd6ba2bd4c95566c8450
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2017-02-13 15:51:52 -0600

    Changed to a new bot framework

---
+++ classes/DungeonBot.php
@@ -0,0 +1,16 @@
+<?php
+namespace App;
+
+/**
+* 
+*/
+class DungeonBot
+{
+	
+	// function __construct(argument)
+	// {
+	// 	# code...
+	// }
+}
+
+?>
--- index.php
+++ index.php
@@ -1,15 +1,30 @@
 <?php
 
-namespace App;
 
 require_once './vendor/autoload.php';
-require_once 'BotAuth.php';
+// require_once './classes/BotAuth.php';
+// require_once './classes/DungeonBot.php';
 
-use DanySpin97\PhpBotFramework\Bot;
+use Telegram\Bot\Api;
+use App\BotAuth;
+
 
 $auth = new BotAuth();
-$bot = new Bot($auth->getAuth());
+$telegram = new Api($auth->getAuthToken());
 
-echo "test";
+
+$telegram->addCommand(Commands\StartCommand::class);
+
+$updates = $telegram->getWebHookUpdates();
+
+// $response = $telegram->getMe();
+
+// $botId = $response->getId();
+// $firstName = $response->getFirstName();
+// $username = $response->getUsername();
+
+// echo "ID: " .$botId . "<br>";
+// echo "firstName: " .$firstName . "<br>";
+// echo "username: " .$username . "<br>";
 
 ?>
