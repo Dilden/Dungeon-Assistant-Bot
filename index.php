a77a77d1834bfb0151c55b69a36212761cf68872
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2017-02-17 14:11:40 -0600

    trying autoloading another way

--- index.php
+++ index.php
@@ -7,6 +7,8 @@ use Telegram\Bot\Api;
 use App\BotAuth;
 use App\commands\StartCommand;
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 $auth = new BotAuth();
 $telegram = new Api($auth->getAuthToken());
