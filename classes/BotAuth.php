68595678b371369fefe90dd03f5bf70692981801
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2017-02-17 18:00:45 -0600

    omfg...

---
+++ classes/BotAuth.php
@@ -0,0 +1,24 @@
+<?php
+namespace derp;
+
+class BotAuth {
+	const auth_token = "272639783:AAGBvtV8s2YW-PQgNGrY2AlSabWGVxweHoM";
+
+	public function getAuthToken() {
+		return self::auth_token;
+	}
+
+	public function setWebhook() {
+		$curl = curl_init("https://api.telegram.org:443/bot".self::auth_token."/setwebhook?url=https://wild-mole-8374.vagrantshare.com/");
+		$result = curl_exec($curl);
+		curl_close($curl);
+		
+		return $result;
+	}
+}
+
+
+// https://api.telegram.org:443/bot272639783:AAGBvtV8s2YW-PQgNGrY2AlSabWGVxweHoM/setwebhook?url=https://salty-shelf-12588.herokuapp.com/index.php
+
+
+?>
